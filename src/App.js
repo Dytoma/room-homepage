8be7564c9662f1243725d7ec2d4cdde4fee9b097
Dytoma <dytomaomar@gmail.com>
2023-01-10 22:08:49 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -44,14 +44,14 @@ function App() {
   }
 
   return (
-    <div className="App min-h-full font-leagueSpartan grid md:grid-rows-[534px_1fr]">
+    <div className="App min-h-full font-leagueSpartan grid md:grid-rows-[2fr_1fr] lg:grid-rows-[534px_1fr]">
 
-      <div className='md:grid md:grid-cols-[840px_1fr] flex flex-col'>
+      <div className='md:grid md:grid-cols-[2fr_1fr] lg:grid-cols-[840px_1fr] flex flex-col'>
 
-        <div className='hero-section relative w-full'>
+        <div className='hero-section relative w-full h-full'>
           <Slide left>
-            <img src={items[item].image} alt="" className='hidden md:inline-block'/>
-            <img src={items[item].imageMobile} alt="" className='md:hidden inline-block w-full'/>
+            <img src={items[item].image} alt="" className='hidden md:inline-block object-fill'/>
+            <img src={items[item].imageMobile} alt="" className='md:hidden inline-block w-full object-fill'/>
           </Slide>
 
           <Fade down>
@@ -67,42 +67,43 @@ function App() {
               <img src={ArrowRight} alt="" aria-hidden="true" />
             </button>
           </div>
+          
         </div>
         <div className='relative'>
           <Slide right>
-            <div className='md:pt-[120px] md:pl-[100px] md:pr-[45px] pb-20 pt-12 px-6'>
+            <div className='lg:pt-[120px] md:py-20  md:px-8 lg:pl-[100px] lg:pr-[45px] lg:pb-20 pb-20 md:pb-10 pt-12 px-6'>
               <h1 className='text-4xl md:text-5xl font-semibold tracking-tighter'>{items[item].itemHeader}</h1>
               <p className='text-base leading-6 tracking-tight font medium mt-5 text-darkGray'>{items[item].itemDesc}</p>
 
-              <div className='mt-7'>
-                <a href="#shop" className='flex space-x-8 hover:text-darkGray transition-all duration-300 ease-in-out'>
+              <div className=' mt-7'>
+                <a href="#shop" className='flex md:space-x-5 lg:space-x-8 hover:text-darkGray transition-all duration-300 ease-in-out'>
                   <h3 className='text-base font-medium tracking-[0.735em] leading-4'>SHOP NOW</h3>
-                  <img src={Arrow} alt="" aria-hidden='true'/>
+                  <img src={Arrow} alt="" aria-hidden='true' className=''/>
                 </a>
               </div>
             </div>
           </Slide>
 
           <div className='bg-black absolute bottom-0 left-0 hidden md:inline-block'>
-            <button aria-label='Left item' className='py-7 px-8 hover:bg-veryDarkGray transition-all duration-300 ease-in-out' onClick={prevItem}>
+            <button aria-label='Left item' className='lg:py-7 lg:px-8 md:px-5 md:py-4 hover:bg-veryDarkGray transition-all duration-300 ease-in-out' onClick={prevItem}>
               <img src={ArrowLeft} alt="" aria-hidden='true'/>
             </button>
 
-            <button aria-label='right item' className='py-7 px-8 hover:bg-veryDarkGray transition-all duration-300 ease-in-out' onClick={nextItem}>
+            <button aria-label='right item' className='lg:py-7 lg:px-8 md:px-5 md:py-4 hover:bg-veryDarkGray transition-all duration-300 ease-in-out' onClick={nextItem}>
               <img src={ArrowRight} alt="" aria-hidden="true" />
             </button>
           </div>
         </div>
       </div>
 
-      <div className='md:grid md:grid-cols-[420px_1fr_440px] flex flex-col'>
+      <div className='md:grid md:grid-cols-[1fr_1fr_1fr] lg:grid-cols-[420px_1fr_440px] flex flex-col'>
         <Slide left>
           <div>
             <img src={AboutDark} alt="" className='w-full'/>
           </div>
         </Slide>
         <Fade down>
-          <div className='px-8 md:pl-12 md:pt-[68px] md:pr-11 py-16 md:pb-0'>
+          <div className='px-8 md:px-6 lg:pl-12 md:pt-[68px] lg:pr-11 py-16 md:pb-0'>
             <h3 className='text-base font-semibold uppercase tracking-[0.3em] md:tracking-[0.425em] leading-4 mb-4'>
               About our furniture
             </h3>
